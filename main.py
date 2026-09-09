2b8e7a6ca76d52b87d33f2c6219c19f8023df348
dmae1699 <dmae1699@gmail.com>
2026-09-09 20:07:03 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+set1 = {'blue', 'green'} 
+set2 = {'blue', 'yellow'}
+set3 = {1, 2, 3, 4, 5} 
+set4 = {1, 5, 6, 7, 8, 9}
+print(*set1.symmetric_difference(set2))
+print(*set3.symmetric_difference(set4))
